04744b140e52ab851f3ac191d6599b2f8424d4da
Streakman <46490210+Streakman@users.noreply.github.com>
2020-09-07 22:38:41 -0400

    Update gamestate.py

--- gamestate.py
+++ gamestate.py
@@ -1,34 +1,44 @@
 class Player:
     def __init__(self, id, sentence, emails):
-        self.id = ""
-        self.sentence = ""
-        self.emails = ["corneliusregent@gmail.com"]
+        self.id = id
+        self.sentence = sentence
+        self.emails = emails
+
 
 A = Player("0", "hello world!", ["wonderful!"])
-# graph = {
-#   'A' : ['B','C'],
-#   'B' : ['D', 'E'],
-#   'C' : ['F'],
-#   'D' : [],
-#   'E' : ['F'],
-#   'F' : []
-# }
-#
-# visited = [] # List to keep track of visited nodes.
-# queue = []     #Initialize a queue
-#
-# def bfs(visited, graph, node):
-#   visited.append(node)
-#   queue.append(node)
-#
-#   while queue:
-#     s = queue.pop(0)
-#     print (s, end = " ")
-#
-#     for neighbour in graph[s]:
-#       if neighbour not in visited:
-#         visited.append(neighbour)
-#         queue.append(neighbour)
-#
-# # Driver Code
-# bfs(visited, graph, 'A')
+B = Player("1", "as long as it's uncensored", ["wonderful!"])
+C = Player("2", "Yeah well same with my parents", ["wonderful!"])
+E = Player("3", "I dunno my man", ["wonderful!"])
+F = Player("4", "world hello@", ["wonderful!"])
+
+players = {
+    A: [B, C, E, F],
+    B: [C, E],
+    F: [E],
+    C: [],
+}
+
+visited = []  # List to keep track of visited nodes.
+queue = []  # Initialize a queue
+
+"""
+This ain't doing exactly what we want it to do: 
+It's giving us 1,2,3,4 and then 2,3, 3, then nothing but we their parents to appear in all of them
+"""
+def bfs(visited, graph, node):
+    visited.append(node)
+    queue.append(node)
+    #neighbourhood = graph[node]
+    while queue:
+        s = queue.pop(0)
+        if s in graph.keys():
+            for neighbour in graph[s]:
+                #if neighbour not in visited:
+                visited.append(neighbour)
+                queue.append(neighbour)
+                print(neighbour.id)
+            print("||\n")
+
+
+# Driver Code
+bfs(visited, players, A)
